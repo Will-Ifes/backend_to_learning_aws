0a6e812257c5743e56bd5c3c0c2df5cb645d268d
Wildemberg-Otus7 <wildemberg.oliveira@otus7.com>
2024-10-09 16:04:06 -0300

    main  * adjust cors
    Signed-off-by: Wildemberg-Otus7 <wildemberg.oliveira@otus7.com>

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,16 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(';') ?? ['*'];
 
 const corsOptions = {
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    if (!origin || allowedOrigins.includes('*') || allowedOrigins.includes(origin)) {
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    const allowed = allowedOrigins.some((allowedOrigin) => {
+      const regex = new RegExp(`^${allowedOrigin.replace(/\./g, '\\.').replace(/\*/g, '.*')}`);
+      return regex.test(origin);
+    });
+
+    if (allowed) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
