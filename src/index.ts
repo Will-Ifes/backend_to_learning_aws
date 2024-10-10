b4b5ee5a87d7d2acff724c0747d160d1ab3dd887
Wildemberg-Otus7 <wildemberg.oliveira@otus7.com>
2024-10-10 01:54:58 -0300

    main  * REMOVENDO (adicionando https para testes com certificado fake autoassinado)
    Signed-off-by: Wildemberg-Otus7 <wildemberg.oliveira@otus7.com>

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import cors from 'cors';
 import authRoutes from './routes/auth';
 import { authenticateToken } from './middleware/auth';
 import swaggerApp from './swagger';
-import https from 'https';
-import fs from 'fs';
-import path from 'path';
 
 const app = express();
 require('dotenv').config();
@@ -57,16 +54,7 @@ app.use(
   },
 );
 
-// Configurações de certificado
-const options = {
-  key: fs.readFileSync(path.join(__dirname, '../server.key')), // Ajuste para acessar a raiz
-  cert: fs.readFileSync(path.join(__dirname, '../server.cert')) // Ajuste para acessar a raiz
-};
-
-// Porta padrão HTTPS
-const PORT = process.env.PORT || 443; 
-
-// Criar servidor HTTPS
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`Backend running at https://${process.env.BACKEND_URL}`);
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`Backend running at ${process.env.BACKEND_URL}`);
 });
